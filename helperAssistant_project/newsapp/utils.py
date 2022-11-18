72706eab4448e33e155459183f2c94224f7d99ad
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2022-11-18 11:34:12 +0200

    add news

--- helperAssistant_project/newsapp/utils.py
+++ helperAssistant_project/newsapp/utils.py
@@ -7,18 +7,31 @@ HEADERS = {
 }
 
 
-def get_articles():
+def get_category():
     response = requests.get('https://www.unian.ua/', headers=HEADERS).text
     soup = BeautifulSoup(response, 'lxml')
     articles = soup.find(class_="main-menu__content nano").find_all('a')
-    news_dict = {}
+    category_dict = {}
     for article in articles:
         title = article.text.strip()
         link = article.get('href')
         if 'unian.ua' in link:
-            news_dict[title] = link
+            category_dict[title] = link
+    return category_dict
+
+
+def get_news():
+    response = requests.get('https://www.unian.ua/', headers=HEADERS).text
+    soup = BeautifulSoup(response, 'lxml')
+    news_list = soup.find(class_="newsfeed__list").find_all('a')
+    news_dict = {}
+    for news in news_list:
+        title = news.text.strip()
+        link = news.get('href')
+        news_dict[title] = link
     return news_dict
 
 
 if __name__ == '__main__':
-    get_articles()
+    # get_articles()
+    get_news()
--- helperAssistant_project/newsapp/views.py
+++ helperAssistant_project/newsapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from .utils import get_articles
+from .utils import get_category, get_news
 
 
 # Create your views here.
 def news_list(request):
-    news_dict = get_articles()
-    return render(request, 'newsapp/news.html', {'news_dict': news_dict})
+    category_dict = get_category()
+    news_dict = get_news()
+    return render(request, 'newsapp/news.html', {'category_dict': category_dict, 'news_dict': news_dict})
