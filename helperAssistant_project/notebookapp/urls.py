90afa7d47582d56a62c43368b82f1db5075afdc8
Arsen Badia <arsenbadya@gmail.com>
2022-11-18 22:06:55 +0200

    first start
    templates fixed, success

--- helperAssistant_project/notebookapp/urls.py
+++ helperAssistant_project/notebookapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('', views.main, name='main'),
-    path('tag/', views.tag, name='usertag'),
     path('', views.note, name='note'),
+    path('tag/', views.tag, name='usertag'),
     path('detail/<int:note_id>', views.detail, name='detail'),
     path('done/<int:note_id>', views.set_done, name='set_done'),
     path('delete/<int:note_id>', views.delete_note, name='delete_note'),
